f9de165881b112131b7dbbce672bf7cb32d1438a
lizliz404 <liz041218888@gmail.com>
2026-07-20 19:30:12 +0800

    remove experimental /preview route
    Move haoqi-class design lab off the personal site path. Lab now lives as an independent Cloudflare Pages project on lab.lizliz.xyz.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/preview",
     },
     sitemap: absoluteUrl("/sitemap.xml"),
   };
